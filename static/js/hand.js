515295487448af29f096d7f17c38c01b1680b859
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2017-12-27 21:37:19 -0500

    clean up checks

--- static/js/hand.js
+++ static/js/hand.js
@@ -1,104 +1,100 @@
 $('body').on('click', '.card', function(event) {
   event.stopPropagation(); // prevent default bootstrap behavior
-  if(13 <= $('.card.active').length) {
-    if(true == $(this).hasClass('active')) {
+  if(13 > $('.card.active').length || true == $(this).hasClass('active')) {
       $(this).toggleClass('active');
-    }
-  } else {
-    $(this).toggleClass('active');
-    hcp = 0;
-    dip = 0;
-    dup = 0;
-    s = 0;
-    h = 0;
-    c = 0;
-    d = 0;
-    bal = true;
-    $('.card').each(function(k,v) {
-      if($(v).hasClass('active')) {
-        rank = v.id.substring(0,1);
-        suit = v.id.substring(1);
-        switch(rank) {
-          case 'j':
-            hcp++;
-            break;
-          case 'q':
-            hcp += 2;
-            break;
-          case 'k':
-            hcp += 3;
-            break;
-          case 'a':
-            hcp += 4;
-            break;
-        }
-        switch(suit) {
-          case 's':
-            s++;
-            break;
-          case 'h':
-            h++;
-            break;
-          case 'd':
-            d++;
-            break;
-          case 'c':
-            c++;
-            break;
-        }
-      }
-    });
-    if(s > 4) {
-      dip += s - 4;
-    }
-    if(h > 4) {
-      dip += h - 4;
-    }
-    if(d > 4) {
-      dip += d - 4;
-    }
-    if(c > 4) {
-      dip += c - 4;
-    }
-    litmus = 0;
-    if(s == 0) {
-      dup += 5;
-    } else if(s == 1) {
-      dup += 3;
-    } else if(s == 2) {
-      dup += 1;
-      litmus++;
-    }
-    if(h == 0) {
-      dup += 5;
-    } else if(h == 1) {
-      dup += 3;
-    } else if(h == 2) {
-      dup += 1;
-      litmus++;
-    }
-    if(d == 0) {
-      dup += 5;
-    } else if(d == 1) {
-      dup += 3;
-    } else if(d == 2) {
-      dup += 1;
-      litmus++;
-    }
-    if(c == 0) {
-      dup += 5;
-    } else if(c == 1) {
-      dup += 3;
-    } else if(c == 2) {
-      dup += 1;
-      litmus++;
-    }
-    if(litmus > 1 || s < 2 || h < 2 || d < 2 || c < 2) {
-      bal = false;
-    }
-    $('#hcp').text(hcp);
-    $('#dip').text(dip);
-    $('#dup').text(dup);
-    $('#bal').text(bal ? 'Y' : 'N');
   }
+  hcp = 0;
+  dip = 0;
+  dup = 0;
+  s = 0;
+  h = 0;
+  c = 0;
+  d = 0;
+  bal = true;
+  $('.card').each(function(k,v) {
+    if($(v).hasClass('active')) {
+      rank = v.id.substring(0,1);
+      suit = v.id.substring(1);
+      switch(rank) {
+        case 'j':
+          hcp++;
+          break;
+        case 'q':
+          hcp += 2;
+          break;
+        case 'k':
+          hcp += 3;
+          break;
+        case 'a':
+          hcp += 4;
+          break;
+      }
+      switch(suit) {
+        case 's':
+          s++;
+          break;
+        case 'h':
+          h++;
+          break;
+        case 'd':
+          d++;
+          break;
+        case 'c':
+          c++;
+          break;
+      }
+    }
+  });
+  if(s > 4) {
+    dip += s - 4;
+  }
+  if(h > 4) {
+    dip += h - 4;
+  }
+  if(d > 4) {
+    dip += d - 4;
+  }
+  if(c > 4) {
+    dip += c - 4;
+  }
+  litmus = 0;
+  if(s == 0) {
+    dup += 5;
+  } else if(s == 1) {
+    dup += 3;
+  } else if(s == 2) {
+    dup += 1;
+    litmus++;
+  }
+  if(h == 0) {
+    dup += 5;
+  } else if(h == 1) {
+    dup += 3;
+  } else if(h == 2) {
+    dup += 1;
+    litmus++;
+  }
+  if(d == 0) {
+    dup += 5;
+  } else if(d == 1) {
+    dup += 3;
+  } else if(d == 2) {
+    dup += 1;
+    litmus++;
+  }
+  if(c == 0) {
+    dup += 5;
+  } else if(c == 1) {
+    dup += 3;
+  } else if(c == 2) {
+    dup += 1;
+    litmus++;
+  }
+  if(litmus > 1 || s < 2 || h < 2 || d < 2 || c < 2) {
+    bal = false;
+  }
+  $('#hcp').text(hcp);
+  $('#dip').text(dip);
+  $('#dup').text(dup);
+  $('#bal').text(bal ? 'Y' : 'N');
 });

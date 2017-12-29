89f7501fe7572dccf1ed51d08d1c518aec73ffe6
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2017-12-29 14:16:12 -0500

    working draft

--- static/js/story_library.js
+++ static/js/story_library.js
@@ -1,11 +1,103 @@
 var story_library = {
   'opens' : {
-    '0n' : 'I have less than 13 HCPDi. I do not have a 6+ Good suit. It is unclear if my hand is balanced.',
-    '1c' : 'I have between 13 and 21 HCPDi. I have 3+ &#x2663;. I have < 5 &#x2660; &#x0026; <span class="red">&#x2665;<span> and < 3 <span class="red">&#x2666;</span>. It is unclear if my hand is balanced. My bid was Invitational.',
-    '1d' : 'I have between 13 and 21 HCPDi. I have 3+ <span class="red">&#x2666;</span>. I have < 5 &#x2660; &#x0026; <span class="red">&#x2665;<span>. It is unclear how many &#x2663; I have. It is unclear if my hand is balanced. My bid was Invitational.',
-    '1h' : 'I have between 13 and 21 HCPDi. I have 5+ <span class="red">&#x2665;</span>. I have < 5 &#x2660;. It is unclear how many <span class="red">&#x2666;<span> &#x0026; &#x2663; I have. It is unclear if my hand is balanced. My bid was Invitational.',
-    '1s' : 'I have between 13 and 21 HCPDi. I have 5+ &#x2660;. It is unclear how many <span class="red">&#x2665;</span>, <span class="red">&#x2666;<span>, &#x0026; &#x2663; I have. It is unclear if my hand is balanced. My bid was Invitational.',
-    '1n' : 'I have between 15 and 17 HCPDi. My hand is balanced. It is unclear how many &#x2660;, <span class="red">&#x2665;</span>, <span class="red">&#x2666;<span>, &#x0026; &#x2663; I have. My bid was Invitational.',
-    '2c' : 'I have 22+ HCPDi. It is unclear how many &#x2660;, <span class="red">&#x2665;</span>, <span class="red">&#x2666;<span>, &#x0026; &#x2663; I have. It is unclear if myhand is balanced. My bid was Artificial. My bid was Forcing.',
+    '0n' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : -1, 's' : '< 6', 'sg' : -1, 'h' : '< 6', 'hg' : -1, 'd' : '< 6', 'dg' : -1, 'c' : '< 6', 'cg' : -1, 'alt' : 0, 'type' : -1, 'conv' : -1 },
+    '1c' : { 'hcpdi' : 'between 13 &#x0026; 21', 'hcpdu' : -1, 'bal' : -1, 's' : '< 5', 'sg' : -1, 'h' : '< 5', 'hg' : -1, 'd' : '< 3', 'dg' : -1, 'c' : '3+', 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+    '1d' : { 'hcpdi' : 'between 13 &#x0026; 21', 'hcpdu' : -1, 'bal' : -1, 's' : '< 5', 'sg' : -1, 'h' : '< 5', 'hg' : -1, 'd' : '3+', 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+    '1h' : { 'hcpdi' : 'between 13 &#x0026; 21', 'hcpdu' : -1, 'bal' : -1, 's' : '< 5', 'sg' : -1, 'h' : '5+', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+    '1s' : { 'hcpdi' : 'between 13 &#x0026; 21', 'hcpdu' : -1, 'bal' : -1, 's' : '5+', 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+    '1s' : { 'hcpdi' : 'between 15 &#x0026; 17', 'hcpdu' : -1, 'bal' : 1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+    '2c' : { 'hcpdi' : '22+', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : 'Strong 2&#x2663;' },
+    '2d' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : 6, 'dg' : 1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '2h' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : 6, 'hg' : 1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '2s' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : 6, 'sg' : 1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '2n' : { 'hcpdi' : 'between 20 &#x0026; 21', 'hcpdu' : -1, 'bal' : 1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+    '3c' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : 7, 'cg' : 1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '3d' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : 7, 'dg' : 1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '3h' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : 7, 'hg' : 1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '3s' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : 7, 'sg' : 1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '3n' : { 'alt' : -1 },
+    '4c' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : 8, 'cg' : 1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '4d' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : 8, 'dg' : 1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '4h' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : '8+', 'hg' : 1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '4s' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : '8+', 'sg' : 1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '4n' : { 'hcpdi' : '25+', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : 'Blackwood' },
+    '5c' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : '9+', 'cg' : 1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '5d' : { 'hcpdi' : '< 13', 'hcpdu' : -1, 'bal' : 0, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : '9+', 'dg' : 1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : 'Pre&#x00EB;mptive' },
+    '5h' : { 'alt' : -1 },
+    '5s' : { 'alt' : -1 },
+    '5n' : { 'alt' : -1 },
+    '6c' : { 'alt' : -1 },
+    '6d' : { 'alt' : -1 },
+    '6h' : { 'alt' : -1 },
+    '6s' : { 'alt' : -1 },
+    '6n' : { 'alt' : -1 },
+    '7c' : { 'alt' : -1 },
+    '7d' : { 'alt' : -1 },
+    '7h' : { 'alt' : -1 },
+    '7s' : { 'alt' : -1 },
+    '7n' : { 'alt' : -1 }
+  },
+  'responses' : {
+    'minor' : {
+      '0n' : { 'hcpdi' : '< 6', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 2, 'conv' : -1 },
+      '1c' : { 'alt' : -1 },
+      '1d' : { 
+        'alt' : {
+          '1c' : { 'hcpdi' : '6+', 'hcpdu' : -1, 'bal' : -1, 's' : '< 4', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : '4+', 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : -1 },
+          'xx' : { 'alt' : -1 }
+        },
+      '1h' : { 'hcpdi' : '6+', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : '4+', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : -1 },
+      '1s' : { 'hcpdi' : '6+', 'hcpdu' : -1, 'bal' : -1, 's' : '4+', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : -1 },
+      '1n' : { 'hcpdi' : 'between 6 &#x0026; 10', 'hcpdu' : -1, 'bal' : 1, 's' : '< 4', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+      '2c' : { 
+        'alt' : {
+          '1c' : { 'hcpdi' : 'between 6 &#x0026; 10', 'hcpdu' : -1, 'bal' : -1, 's' : '< 4', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : '5+', 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+          'xx' : { 'hcpdi' : '11+', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : '5+', 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : -1 }
+        },
+      '2d' : {
+        'alt' : {
+          '1d' : { 'hcpdi' : 'between 6 &#x0026; 10', 'hcpdu' : -1, 'bal' : -1, 's' : '< 4', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : '5+', 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+          'xx' : { 'hcpdi' : '17+', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : '5+', 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : -1 }
+        },
+      '2h' : { 'hcpdi' : '17+', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : '5+', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : -1 },
+      '2s' : { 'hcpdi' : '17+', 'hcpdu' : -1, 'bal' : -1, 's' : '5+', 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : -1 },
+      '2n' : { 'hcpdi' : 'between 11 &#x0026; 12', 'hcpdu' : -1, 'bal' : 1, 's' : '< 4', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+      '3c' : { 
+        'alt' : {
+          '1c' : { 'hcpdi' : 'between 11 &#x0026; 12', 'hcpdu' : -1, 'bal' : -1, 's' : '< 4', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : '4+', 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+          'xx' : { 'hcpdi' : '17+', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : '5+', 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : -1 }
+        },
+      '3d' : {
+        'alt' : {
+          '1d' : { 'hcpdi' : 'between 11 &#x0026; 12', 'hcpdu' : -1, 'bal' : -1, 's' : '< 4', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : '4+', 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+          'xx' : { 'alt' : -1 }
+        },
+      '3h' : { 'alt' : -1 },
+      '3s' : { 'alt' : -1 },
+      '3n' : { 'hcpdi' : 'between 13 &#x0026; 15', 'hcpdu' : -1, 'bal' : 1, 's' : '< 4', 'sg' : -1, 'h' : '< 4', 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 0, 'conv' : -1 },
+      '4c' : { 'alt' : -1 },
+      '4d' : { 'alt' : -1 },
+      '4h' : { 'alt' : -1 },
+      '4s' : { 'alt' : -1 },
+      '4n' : { 'hcpdi' : '20+', 'hcpdu' : -1, 'bal' : -1, 's' : -1, 'sg' : -1, 'h' : -1, 'hg' : -1, 'd' : -1, 'dg' : -1, 'c' : -1, 'cg' : -1, 'alt' : 0, 'type' : 1, 'conv' : 'Blackwood;' },
+      '5c' : { 'alt' : -1 },
+      '5d' : { 'alt' : -1 },
+      '5h' : { 'alt' : -1 },
+      '5s' : { 'alt' : -1 },
+      '5n' : { 'alt' : -1 },
+      '6c' : { 'alt' : -1 },
+      '6d' : { 'alt' : -1 },
+      '6h' : { 'alt' : -1 },
+      '6s' : { 'alt' : -1 },
+      '6n' : { 'alt' : -1 },
+      '7c' : { 'alt' : -1 },
+      '7d' : { 'alt' : -1 },
+      '7h' : { 'alt' : -1 },
+      '7s' : { 'alt' : -1 },
+      '7n' : { 'alt' : -1 }
+    },
+    'major' : {
+      
+    }
   }
 }

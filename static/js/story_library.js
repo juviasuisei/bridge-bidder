6968e080cfef06b5a70d67d6ae05024495ba30fa
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2017-12-29 01:07:43 -0500

    working draft

--- static/js/story_library.js
+++ static/js/story_library.js
@@ -1,2 +1,11 @@
 var story_library = {
+  'opens' : {
+    '0n' : 'I have less than 13 HCPDi. I do not have a 6+ Good suit. It is unclear if my hand is balanced.',
+    '1c' : 'I have between 13 and 21 HCPDi. I have 3+ &#x2663;. I have < 5 &#x2660; &#x0026; <span class="red">&#x2665;<span> and < 3 <span class="red">&#x2666;</span>. It is unclear if my hand is balanced. My bid was Invitational.',
+    '1d' : 'I have between 13 and 21 HCPDi. I have 3+ <span class="red">&#x2666;</span>. I have < 5 &#x2660; &#x0026; <span class="red">&#x2665;<span>. It is unclear how many &#x2663; I have. It is unclear if my hand is balanced. My bid was Invitational.',
+    '1h' : 'I have between 13 and 21 HCPDi. I have 5+ <span class="red">&#x2665;</span>. I have < 5 &#x2660;. It is unclear how many <span class="red">&#x2666;<span> &#x0026; &#x2663; I have. It is unclear if my hand is balanced. My bid was Invitational.',
+    '1s' : 'I have between 13 and 21 HCPDi. I have 5+ &#x2660;. It is unclear how many <span class="red">&#x2665;</span>, <span class="red">&#x2666;<span>, &#x0026; &#x2663; I have. It is unclear if my hand is balanced. My bid was Invitational.',
+    '1n' : 'I have between 15 and 17 HCPDi. My hand is balanced. It is unclear how many &#x2660;, <span class="red">&#x2665;</span>, <span class="red">&#x2666;<span>, &#x0026; &#x2663; I have. My bid was Invitational.',
+    '2c' : 'I have 22+ HCPDi. It is unclear how many &#x2660;, <span class="red">&#x2665;</span>, <span class="red">&#x2666;<span>, &#x0026; &#x2663; I have. It is unclear if myhand is balanced. My bid was Artificial. My bid was Forcing.',
+  }
 }

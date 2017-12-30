2d5f509a0030aea955ed7295675ffcf8c7742d91
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2017-12-30 00:59:43 -0500

    set the bid numbers and bidders correctly.

--- static/js/story.js
+++ static/js/story.js
@@ -1,12 +1,13 @@
 function tellStory() {
   $('#storyacc').empty();
-  bid_count = $('.bids').length
+  bid_count = $('.bids').length - 1;
+  i = 1;
   while(bid_count > 0) {
     panel = '';
     panel += '<div class="panel panel-default storybids">';
     panel += '<div class="panel-heading" role="tab" id="storyacc1h">';
     panel += '<p class="panel-title">';
-    panel += '<a role="button" data-toggle="collapse" data-parent="#storyacc" href="#storyacc1" aria-expanded="false" aria-controls="storyacc1">Bid 1 (' + bidder + '): </a>';
+    panel += '<a role="button" data-toggle="collapse" data-parent="#storyacc" href="#storyacc1" aria-expanded="false" aria-controls="storyacc1">Bid ' + i + ' (' + $('#bid' + i + 'b').text() + '): </a>';
     panel += '</p>';
     panel += '</div>';
     panel += '<div id="storyacc1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="storyacc1">';
@@ -14,5 +15,6 @@ function tellStory() {
     panel += '</div>';
     $('#storyacc').prepend(panel);
     bid_count--;
+    i++;
   }
 }

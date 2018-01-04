a410576da1e5351a3f7e8d1e52562a5862ead9bb
J. A. Mercury <13936649+tsukinoooo@users.noreply.github.com>
2018-01-04 18:44:29 -0500

    remove console log

--- static/js/story.js
+++ static/js/story.js
@@ -207,7 +207,6 @@ function tellStory() {
                     return false;
                   } else {
                     $.each(v2.alt, function(k3, v3) {
-                      console.log(v2.alt, k, k2, k3, v3);
                       if(k3 == openbid) {
                         biddata = v3;
                         litmus = false;
